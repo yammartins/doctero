9a33c8417d6f83d2349a940c262aaa26ab45e585
yumartins <develop@yumartins.com.br>
2021-04-04 19:48:08 -0300

    feat(tailwind): minify bundle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,32 @@
+const plugin = require('tailwindcss/plugin')
+
+const grid = {
+  '.container': {
+    margin: '0 auto',
+    padding: '0 1rem',
+    maxWidth: '100%',
+
+    '@screen xs': {
+      maxWidth: '540px'
+    },
+    '@screen sm': {
+      maxWidth: '720px'
+    },
+    '@screen md': {
+      maxWidth: '960px'
+    },
+    '@screen lg': {
+      maxWidth: '1140px'
+    },
+    '@screen xl': {
+      maxWidth: '1248px'
+    },
+    '@screen xxl': {
+      maxWidth: '1440px'
+    }
+  }
+}
+
 module.exports = {
   purge: [
     'pages/*.vue',
@@ -7,10 +36,97 @@ module.exports = {
   ],
   darkMode: false,
   theme: {
-    extend: {}
+    extend: {},
+
+    screens: {
+      xs: '576px',
+      sm: '768px',
+      md: '992px',
+      lg: '1200px',
+      xl: '1280px',
+      xxl: '1536px'
+    }
   },
+
   variants: {
-    extend: {}
+    rotate: [],
+    zIndex: [],
+    margin: [],
+    padding: [],
+    opacity: [],
+    spacing: [],
+    outline: [],
+    fontSize: ['responsive'],
+    transform: [],
+    translate: [],
+    textColor: [],
+    boxShadow: [],
+    lineHeight: [],
+    fontWeight: [],
+    textOpacity: [],
+    borderWidth: [],
+    borderColor: [],
+    borderRadius: [],
+    borderOpacity: [],
+    letterSpacing: [],
+    textDecoration: [],
+    backgroundColor: [],
+    placeholderColor: [],
+    backgroundOpacity: [],
+    gridTemplateColumns: ['responsive']
   },
-  plugins: []
+
+  plugins: [
+    plugin(({ addComponents }) => addComponents(grid))
+  ],
+
+  corePlugins: {
+    fill: false,
+    skew: false,
+    float: false,
+    scale: false,
+    clear: false,
+    stroke: false,
+    minWidth: false,
+    maxWidth: false,
+    minHeight: false,
+    container: false,
+    maxHeight: false,
+    ringColor: false,
+    ringWidth: false,
+    placeSelf: false,
+    alignSelf: false,
+    visibility: false,
+    placeItems: false,
+    strokeWidth: false,
+    tableLayout: false,
+    ringOpacity: false,
+    divideWidth: false,
+    justifySelf: false,
+    divideColor: false,
+    divideStyle: false,
+    placeContent: false,
+    alignContent: false,
+    justifyItems: false,
+    listStyleType: false,
+    divideOpacity: false,
+    accessibility: false,
+    objectPosition: false,
+    backgroundSize: false,
+    borderCollapse: false,
+    backgroundClip: false,
+    backgroundImage: false,
+    ringOffsetColor: false,
+    ringOffsetWidth: false,
+    transformOrigin: false,
+    placeholderColor: false,
+    backgroundRepeat: false,
+    listStylePosition: false,
+    fontVariantNumeric: false,
+    overscrollBehavior: false,
+    placeholderOpacity: false,
+    backgroundPosition: false,
+    gradientColorStops: false,
+    backgroundAttachment: false
+  }
 }
