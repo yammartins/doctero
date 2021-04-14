e10683798e3bbe8402284fc463db2ef3f64fb18d
Yuri Martins <develop@yumartins.com.br>
2021-04-14 20:48:50 -0300

    feat(views): base header home

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,48 @@
 <template>
-  <div class="container">
-    <h1>Hello World</h1>
+  <div class="home">
+    <div class="container">
+      <div class="home-wrapper">
+        <h1 class="text-5xl font-bold text-gray-600">
+          Build fast, secured and beutuful web-sites.
+        </h1>
+
+        <p class="text-base mt-5 text-gray-400">
+          Create, build, collaborate and ship products faster. Good bye code! 👋
+        </p>
+
+        <Button
+          label="Cadastre-se agora"
+          medium
+          class="mt-16"
+        />
+      </div>
+
+      <img
+        class="home-image"
+        src="~/assets/images/header.png"
+      >
+    </div>
   </div>
 </template>
 
-<script>
-export default {}
-</script>
+<style lang="postcss">
+.home {
+  @apply flex h-screen items-center;
+
+  &-wrapper {
+    @apply max-w-xl;
+
+    p {
+      @apply max-w-sm leading-relaxed;
+    }
+
+    h1 {
+      @apply leading-normal;
+    }
+  }
+
+  .container {
+    @apply flex items-center justify-between;
+  }
+}
+</style>
