de762b5b84ca219f248dddd8397fee72b2ce94ef
Yuri Martins <develop@yumartins.com.br>
2021-04-18 21:03:48 -0300

    feat(views): page signup view

--- apps/web/pages/auth/signup.tsx
+++ apps/web/pages/auth/signup.tsx
@@ -1,3 +1,44 @@
-const Signup: React.FC = () => <h1>Signup</h1>;
+import { useRef } from 'react';
+
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import { Button, FormInput } from '@uxoctopus/core';
+
+import { Auth as Layout } from '../../layouts';
+
+const Signup: React.FC = () => {
+  const ref = useRef<FormHandles>(null);
+
+  return (
+    <Layout
+      type="signup"
+      title="Cadastrar"
+      description="Crie sua conta gratuitamente e em poucos segundos."
+    >
+      <Form
+        ref={ref}
+        onSubmit={() => null}
+      >
+        <FormInput
+          name="name"
+          label="Nome"
+        />
+
+        <FormInput
+          name="email"
+          label="E-mail"
+        />
+
+        <FormInput
+          name="password"
+          label="Senha"
+          isPassword
+        />
+
+        <Button label="Avançar" />
+      </Form>
+    </Layout>
+  );
+};
 
 export default Signup;
