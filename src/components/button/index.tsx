a4b0f72535b7b5de595182b201a10621ed2b512b
Yam <yamprado2010@hotmail.com>
2022-04-28 12:26:30 -0300

    feat: add a new component: button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,13 +1,38 @@
-const Button: React.FC = () => (
-  <button type="button">
-    {icon}
+import classNames from 'classnames';
 
-    {loading ? (
-      <div className="loading" />
-    ) : (
-      label
-    )}
-  </button>
-);
+import { ButtonHandles } from './types';
+
+const Button: React.FC<ButtonHandles> = ({
+  size,
+  full,
+  label,
+  loading,
+  appearance,
+  icon,
+  submit,
+  disabled,
+  ...rest
+}) => {
+  const styled = classNames(
+    'button',
+  );
+
+  return (
+    <button
+      {...rest}
+      type={submit ? 'submit' : 'button'}
+      disabled={disabled}
+      className={styled}
+    >
+      {icon}
+
+      {loading ? (
+        <div className="loading" />
+      ) : (
+        label
+      )}
+    </button>
+  );
+};
 
 export default Button;
