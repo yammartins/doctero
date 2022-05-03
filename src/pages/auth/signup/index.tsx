4eda3c57d4f16744f3c9324db66fb300914f2cb2
Yam <yamprado2010@hotmail.com>
2022-05-03 15:59:06 -0300

    feat: first step to register is done

--- src/pages/auth/signup/index.tsx
+++ src/pages/auth/signup/index.tsx
@@ -1,63 +1,69 @@
+import { useState } from 'react';
+
 import { Form } from '@unform/web';
 
 import { Input, Button } from '~/components';
 import { Auth } from '~/layouts';
 import { Created } from '~/styles';
 
-const SignUp: React.FC = () => (
-  <Auth
-    title="Criar conta"
-    description="Vamos começar por algumas informações essenciais."
-    previous
-  >
-    <Created>
-      <Form
-        onSubmit={() => {}}
-        className="signup-box"
-      >
+const SignUp: React.FC = () => {
+  const [nextStep, onNextStep] = useState(null);
 
-        <div className="input-box">
+  return (
+    <Auth
+      title="Criar conta"
+      description="Vamos começar por algumas informações essenciais."
+      previous
+    >
+      <Created>
+        <Form
+          onSubmit={() => {}}
+          className="signup-box"
+        >
 
-          <Input
-            label="Nome completo"
-            name="name"
-            icon="user"
+          <div className="input-box">
+
+            <Input
+              label="Nome completo"
+              name="name"
+              icon="user"
+            />
+
+            <Input
+              label="E-mail"
+              name="email"
+              icon="email"
+              type="email"
+            />
+
+            <Input
+              label="Senha"
+              name="password"
+              type="password"
+              icon="password"
+            />
+
+            <Input
+              label="Telefone"
+              name="phone"
+              icon="phone"
+              mask="(99) 99999-9999"
+            />
+
+          </div>
+
+          <Button
+            type="submit"
+            label="Criar conta"
+            full
+            appearance="secondary"
           />
 
-          <Input
-            label="E-mail"
-            name="email"
-            icon="email"
-            type="email"
-          />
+        </Form>
+      </Created>
 
-          <Input
-            label="Senha"
-            name="password"
-            type="password"
-            icon="password"
-          />
-
-          <Input
-            label="Telefone"
-            name="phone"
-            icon="phone"
-            mask="(99) 99999-9999"
-          />
-
-        </div>
-
-        <Button
-          type="submit"
-          label="Criar conta"
-          full
-          appearance="secondary"
-        />
-
-      </Form>
-    </Created>
-
-  </Auth>
-);
+    </Auth>
+  );
+};
 
 export default SignUp;
--- src/styles/pages/auth/nextStep.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const View = styled.div`
-  position: relative;
-`;
-
-export default View;
