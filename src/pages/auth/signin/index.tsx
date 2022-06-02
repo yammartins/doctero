810b3433697b97798b077ca4a5b1b1304291510e
Yam <yamprado2010@hotmail.com>
2022-06-02 15:31:58 -0300

    chore: install react-input-mask and @unform

--- src/pages/auth/signin/index.tsx
+++ src/pages/auth/signin/index.tsx
@@ -6,7 +6,6 @@ const SignIn: React.FC = () => {
   return (
     <AuthLayout
      title="Entrar"
-     description="Rebelam-se contra as regras os que não podem cumpri-la. A virtude só é grande quando difícil."
     >
      Inserir o Vasco aqui.
     </AuthLayout>
